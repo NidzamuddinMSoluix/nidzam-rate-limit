da463dee2a2815f907cdb8c7511884bea60d7576
NidzamuddinMSoluix <nidzamuddin.muzakki@soluix.ai>
2023-01-26 17:37:23 +0700

    kl

---
+++ gin/gin.go
@@ -0,0 +1,150 @@
+package gin
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/NidzamuddinMSoluix/nidzam-rate-limit/router"
+
+	"github.com/gin-gonic/gin"
+	"github.com/luraproject/lura/v2/config"
+	"github.com/luraproject/lura/v2/logging"
+	"github.com/luraproject/lura/v2/proxy"
+	krakendgin "github.com/luraproject/lura/v2/router/gin"
+)
+
+// HandlerFactory is the out-of-the-box basic ratelimit handler factory using the default krakend endpoint
+// handler for the gin router
+var HandlerFactory = NewRateLimiterMw(logging.NoOp, krakendgin.EndpointHandler)
+
+// NewRateLimiterMw builds a rate limiting wrapper over the received handler factory.
+func NewRateLimiterMw(logger logging.Logger, next krakendgin.HandlerFactory) krakendgin.HandlerFactory {
+	return func(remote *config.EndpointConfig, p proxy.Proxy) gin.HandlerFunc {
+		logPrefix := "[ENDPOINT: " + remote.Endpoint + "][Ratelimit]"
+		handlerFunc := next(remote, p)
+
+		cfg, err := router.ConfigGetter(remote.ExtraConfig)
+		if err != nil {
+			if err != router.ErrNoExtraCfg {
+				logger.Error(logPrefix, err)
+			}
+			return handlerFunc
+		}
+
+		if cfg.MaxRate <= 0 && cfg.ClientMaxRate <= 0 {
+			return handlerFunc
+		}
+
+		if cfg.MaxRate > 0 {
+			if cfg.Capacity == 0 {
+				if cfg.MaxRate < 1 {
+					cfg.Capacity = 1
+				} else {
+					cfg.Capacity = int64(cfg.MaxRate)
+				}
+			}
+			logger.Debug(logPrefix, "Rate limit enabled")
+			// handlerFunc = NewEndpointRateLimiterMw(juju.NewLimiter(cfg.MaxRate, cfg.Capacity))(handlerFunc)
+			return handlerFunc
+		}
+		if cfg.ClientMaxRate > 0 {
+			if cfg.ClientCapacity == 0 {
+				if cfg.MaxRate < 1 {
+					cfg.ClientCapacity = 1
+				} else {
+					cfg.ClientCapacity = int64(cfg.ClientMaxRate)
+				}
+			}
+			logger.Debug(logPrefix, "coba")
+			handlerFunc = NewTokenLimiterMw()(handlerFunc)
+			// switch strategy := strings.ToLower(cfg.Strategy); strategy {
+			// case "ip":
+			// 	logger.Debug(logPrefix, "IP-based rate limit enabled")
+			// 	handlerFunc = NewIpLimiterWithKeyMw(cfg.Key, cfg.ClientMaxRate, cfg.ClientCapacity)(handlerFunc)
+			// case "header":
+			// 	logger.Debug(logPrefix, "Header-based rate limit enabled")
+			// 	handlerFunc = NewHeaderLimiterMw(cfg.Key, cfg.ClientMaxRate, cfg.ClientCapacity)(handlerFunc)
+			// default:
+			// 	logger.Warning(logPrefix, "Unknown strategy", strategy)
+			// }
+		}
+		return handlerFunc
+	}
+}
+
+// EndpointMw is a function that decorates the received handlerFunc with some rateliming logic
+type EndpointMw func(gin.HandlerFunc) gin.HandlerFunc
+
+// // NewEndpointRateLimiterMw creates a simple ratelimiter for a given handlerFunc
+
+// // NewHeaderLimiterMw creates a token ratelimiter using the value of a header as a token
+// func NewHeaderLimiterMw(header string, maxRate float64, capacity int64) EndpointMw {
+// 	return NewTokenLimiterMw(HeaderTokenExtractor(header), juju.NewMemoryStore(maxRate, capacity))
+// }
+
+// // NewIpLimiterMw creates a token ratelimiter using the IP of the request as a token
+// func NewIpLimiterMw(maxRate float64, capacity int64) EndpointMw {
+// 	return NewTokenLimiterMw(IPTokenExtractor, juju.NewMemoryStore(maxRate, capacity))
+// }
+
+// // NewIpLimiterWithKeyMw creates a token ratelimiter using the IP of the request as a token
+// func NewIpLimiterWithKeyMw(header string, maxRate float64, capacity int64) EndpointMw {
+// 	if header == "" {
+// 		return NewIpLimiterMw(maxRate, capacity)
+// 	}
+// 	return NewTokenLimiterMw(NewIPTokenExtractor(header), juju.NewMemoryStore(maxRate, capacity))
+// }
+
+// // TokenExtractor defines the interface of the functions to use in order to extract a token for each request
+// type TokenExtractor func(*gin.Context) string
+
+// // IPTokenExtractor extracts the IP of the request
+// func IPTokenExtractor(c *gin.Context) string { return c.ClientIP() }
+
+// // NewIPTokenExtractor generates an IP TokenExtractor checking first for the contents of the passed header.
+// // If nothing is found there, the regular IPTokenExtractor function is called.
+// func NewIPTokenExtractor(header string) TokenExtractor {
+// 	return func(c *gin.Context) string {
+// 		if clientIP := strings.TrimSpace(strings.Split(c.Request.Header.Get(header), ",")[0]); clientIP != "" {
+// 			ip := strings.Split(clientIP, ":")[0]
+// 			if parsedIP := net.ParseIP(ip); parsedIP != nil {
+// 				return ip
+// 			}
+// 		}
+// 		return IPTokenExtractor(c)
+// 	}
+// }
+
+// // HeaderTokenExtractor returns a TokenExtractor that looks for the value of the designed header
+// func HeaderTokenExtractor(header string) TokenExtractor {
+// 	return func(c *gin.Context) string { return c.Request.Header.Get(header) }
+// }
+
+// // NewTokenLimiterMw returns a token based ratelimiting endpoint middleware with the received TokenExtractor and LimiterStore
+// func NewTokenLimiterMw(tokenExtractor TokenExtractor, limiterStore krakendrate.LimiterStore) EndpointMw {
+// 	return func(next gin.HandlerFunc) gin.HandlerFunc {
+// 		return func(c *gin.Context) {
+// 			tokenKey := tokenExtractor(c)
+// 			if tokenKey == "" {
+// 				c.AbortWithError(http.StatusTooManyRequests, krakendrate.ErrLimited)
+// 				return
+// 			}
+// 			if !limiterStore(tokenKey).Allow() {
+// 				c.AbortWithError(http.StatusTooManyRequests, krakendrate.ErrLimited)
+// 				return
+// 			}
+// 			next(c)
+// 		}
+// 	}
+// }
+
+func NewTokenLimiterMw() EndpointMw {
+	return func(next gin.HandlerFunc) gin.HandlerFunc {
+		return func(c *gin.Context) {
+			errr := errors.New("too many request")
+			c.AbortWithError(http.StatusTooManyRequests, errr)
+
+			// next(c)
+		}
+	}
+}
---
+++ router/router.go
@@ -0,0 +1,90 @@
+package router
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/luraproject/lura/v2/config"
+)
+
+// Namespace is the key to use to store and access the custom config data for the router
+const Namespace = "github.com/NidzamuddinMSoluix/nidzam-rate-limit"
+
+// Config is the custom config struct containing the params for the router middlewares
+type Config struct {
+	MaxRate        float64
+	Capacity       int64
+	Strategy       string
+	ClientMaxRate  float64
+	ClientCapacity int64
+	Key            string
+}
+
+// ZeroCfg is the zero value for the Config struct
+var ZeroCfg = Config{}
+
+var (
+	ErrNoExtraCfg    = errors.New("no extra config")
+	ErrWrongExtraCfg = errors.New("wrong extra config")
+)
+
+// ConfigGetter parses the extra config for the rate adapter and
+// returns a ZeroCfg and an error if something goes wrong.
+func ConfigGetter(e config.ExtraConfig) (Config, error) {
+	v, ok := e[Namespace]
+	if !ok {
+		return ZeroCfg, ErrNoExtraCfg
+	}
+	tmp, ok := v.(map[string]interface{})
+	if !ok {
+		return ZeroCfg, ErrWrongExtraCfg
+	}
+	cfg := Config{}
+	if v, ok := tmp["max_rate"]; ok {
+		switch val := v.(type) {
+		case int64:
+			cfg.MaxRate = float64(val)
+		case int:
+			cfg.MaxRate = float64(val)
+		case float64:
+			cfg.MaxRate = val
+		}
+	}
+	if v, ok := tmp["capacity"]; ok {
+		switch val := v.(type) {
+		case int64:
+			cfg.Capacity = val
+		case int:
+			cfg.Capacity = int64(val)
+		case float64:
+			cfg.Capacity = int64(val)
+		}
+	}
+	if v, ok := tmp["strategy"]; ok {
+		cfg.Strategy = fmt.Sprintf("%v", v)
+	}
+	if v, ok := tmp["client_max_rate"]; ok {
+		switch val := v.(type) {
+		case int64:
+			cfg.ClientMaxRate = float64(val)
+		case int:
+			cfg.ClientMaxRate = float64(val)
+		case float64:
+			cfg.ClientMaxRate = val
+		}
+	}
+	if v, ok := tmp["client_capacity"]; ok {
+		switch val := v.(type) {
+		case int64:
+			cfg.ClientCapacity = val
+		case int:
+			cfg.ClientCapacity = int64(val)
+		case float64:
+			cfg.ClientCapacity = int64(val)
+		}
+	}
+	if v, ok := tmp["key"]; ok {
+		cfg.Key = fmt.Sprintf("%v", v)
+	}
+	return cfg, nil
+}
